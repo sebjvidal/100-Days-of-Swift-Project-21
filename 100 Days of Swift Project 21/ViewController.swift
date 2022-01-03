00b41ad379315d429f3afd76eee4647ed8d58ea3
sebjvidal <seb.vidal@btinternet.com>
2022-01-03 17:08:00 +0000

    Initial Commit

---
+++ 100 Days of Swift Project 21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  100 Days of Swift Project 21
+//
+//  Created by Seb Vidal on 03/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
